567ff6364df0baa9f093616be345eba0e8c31411
Artecxyz <artecxyz404@gmail.com>
2026-09-11 17:36:15 +0700

    lz4: upgrading to upstream v1.10.0
    Signed-off-by: Artecxyz <artecxyz404@gmail.com>

--- fs/f2fs/compress.c
+++ fs/f2fs/compress.c
@@ -288,8 +288,13 @@ static int lz4_decompress_pages(struct decompress_io_ctx *dic)
 {
 	int ret;
 
+#if defined(CONFIG_ARM64) && defined(CONFIG_KERNEL_MODE_NEON)
+	ret = LZ4_arm64_decompress_safe(dic->cbuf->cdata, dic->rbuf,
+						dic->clen, dic->rlen, false);
+#else
 	ret = LZ4_decompress_safe(dic->cbuf->cdata, dic->rbuf,
-						dic->clen, dic->rlen);
+						dic->clen, dic->rlen, false);
+#endif
 	if (ret < 0) {
 		f2fs_err_ratelimited(dic->sbi,
 				"lz4 decompress failed, ret:%d", ret);
